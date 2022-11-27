b3f1326eb2296c5faf17db560cccc29a4d41785c
ValentinAlvarez98 <valentinalvarez1998@gmail.com>
2022-11-26 22:42:09 -0300

    Ordenando el código

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 let usuarios = [];
 let contrasenas = [];
 let opcionMenu = 0;
@@ -12,6 +13,7 @@ let credito = 0;
 let interes = 0;
 
 function Producto(nombreProd, precioProd, categoriaProd, marcaProd, codigoProd) {
+
     this.nombreProd = nombreProd;
     this.precioProd = precioProd;
     this.categoriaProd = categoriaProd;
@@ -19,10 +21,13 @@ function Producto(nombreProd, precioProd, categoriaProd, marcaProd, codigoProd) 
     this.codigoProd = codigoProd;
 
     this.productoSeleccionado = function () {
+
         alert(`Usted a seleccionado una unidad del producto: \n` +
             this.nombreProd + `\n`
         );
+
     }
+
 }
 
 const producto1 = new Producto("ZOTAC GEFORCE RTX 2060", 619, "TARJETAS DE VIDEO", "NVIDIA", "BFXZON02");
@@ -35,6 +40,7 @@ function carrito() {
     let precioAPagar = 0;
     
     do {
+
     opcionProductos = Number(prompt(`Seleccione los productos deseados: \n
     1) Tarjeta de video:` + " " + `ZOTAC GEFORCE RTX 2060 \n 
     USD 619,00 \n
@@ -47,32 +53,45 @@ function carrito() {
     0) Volver \n`));
         
         switch (opcionProductos) {
-        case 1:
+
+            case 1:
+                
                 producto1.productoSeleccionado();
                 precioAPagar = producto1.precioProd;
                 pago(opcionMetodoPago);
+
             break;
         
-        case 2:
+            case 2:
+                
                 producto2.productoSeleccionado();
                 precioAPagar = producto2.precioProd;
                 pago(opcionMetodoPago);
+
             break;
             
-        case 3:
+            case 3:
+                
                 producto3.productoSeleccionado();
                 precioAPagar = producto3.precioProd;
                 pago(opcionMetodoPago);
+
             break;
             
-        case 4:
+            case 4:
+                
                 producto4.productoSeleccionado();
                 precioAPagar = producto4.precioProd;
                 pago(opcionMetodoPago);
+
             break;
         
-        case 0:
-            alert("¡Volviendo al menú de inicio!");
+            case 0:
+                
+                alert("¡Volviendo al menú de inicio!");
+                
+            break;
+            
         }
         
     } while (opcionProductos != 0);
@@ -81,30 +100,41 @@ function carrito() {
     function pago(opcionMetodoPago) {
 
         do {
+
         opcionMetodoPago = Number(prompt(`Seleccione el método de pago deseado: \n
         1) Efectivo \n 
         2) Crédito (5% Interés) \n
         0) Volver \n`));
         
-        switch (opcionMetodoPago) {
-            case 1:
-                alert(`Usted a seleccionado: \n
-                Efectivo`);
-                efectivo = 1;
-                calculadoraPago();
-                opcionMetodoPago = 0;
+            switch (opcionMetodoPago) {
+            
+                case 1:
+                    
+                    alert(`Usted a seleccionado: \n
+                    Efectivo`);
+
+                    efectivo = 1;
+                    calculadoraPago();
+                    opcionMetodoPago = 0;
+                    
                 break;
             
-            case 2:
-                alert(`Usted a seleccionado: \n
-                Crédito`);
-                credito = 1;
-                calculadoraPago();
-                opcionMetodoPago = 0;
+                case 2:
+                    
+                    alert(`Usted a seleccionado: \n
+                    Crédito`);
+
+                    credito = 1;
+                    calculadoraPago();
+                    opcionMetodoPago = 0;
+                    
                 break;
             
-            case 0:
-                alert("¡Volviendo al menú de productos!");
+                case 0:
+                    
+                    alert("¡Volviendo al menú de productos!");
+                
+                break;
                 
         }
             
@@ -113,25 +143,31 @@ function carrito() {
 
         function calculadoraPago() {
 
-        interes = precioAPagar * (5 / 100);
-        let precioMasInteres = precioAPagar + interes;
-        const precioCuotas = [];
+            interes = precioAPagar * (5 / 100);
+            
+            let precioMasInteres = precioAPagar + interes;
+            const precioCuotas = [];
         
             if (efectivo == 1) {
 
                 alert(`El precio total a pagar es de:\n
                 USD ${precioAPagar}`);
+
                 disponible = Number(prompt(`Ingrese su dinero disponible: `));
 
                 if (disponible >= precioAPagar) {
+
                     alert("¡Compra realizada!");
                     vuelto = disponible - precioAPagar;
                     alert(`Le han sobrado: ${vuelto}`);
                     alert("¡Volviendo al menú de productos!");
                     carrito();
+
                 } else {
+
                     alert("Fondos insuficientes");
                     carrito();
+
                 }
 
             
@@ -139,26 +175,37 @@ function carrito() {
 
                 alert(`El precio total a pagar es de:\n
                 USD ${precioAPagar} + USD ${interes.toFixed(2)} = USD ${precioMasInteres.toFixed(2)}`);
+
                 disponibleTarjeta = Number(prompt(`Ingrese su dinero disponible: `));
 
                 if (disponibleTarjeta >= precioMasInteres) {
+
                     for (let i = 1; i <= 12; i++) {
-                    let precioCuota = precioMasInteres / i;
-                    precioCuotas.push(precioCuota.toFixed(2));
+
+                        let precioCuota = precioMasInteres / i;
+                        precioCuotas.push(precioCuota.toFixed(2));
+
                     }
-                cuotas();
+
+                    cuotas();
+
                 } else {
+
                     alert("Fondos insuficientes");
                     carrito();
+
                 }
             
             } else {
+
                 pago();
-        }
+
+            }
         
         function cuotas() {
 
-        do {
+            do {
+            
             opcionCuotas = Number(prompt(`Seleccione la cantidad de cuotas: \n
             1) 1 Cuota: 1x USD ${precioCuotas[0]} \n
             2) 3 Cuotas: 3x USD ${precioCuotas[2]} \n
@@ -166,44 +213,61 @@ function carrito() {
             4) 12 Cuotas: 12x USD ${precioCuotas[11]} \n
             0) Volver`));
             
-            switch (opcionCuotas) {
-                case 1:
-                    alert(`¡Compra en 1 cuota de USD ${precioCuotas[0]}, realizada!`);
-                    vuelto = disponibleTarjeta - precioMasInteres;
-                    alert(`Le han sobrado: USD ${vuelto.toFixed(2)}`);
-                    alert("¡Volviendo al menú de productos!");
-                    opcionCuotas = 0;
-                    carrito();
+                switch (opcionCuotas) {
+                
+                    case 1:
+                        
+                        alert(`¡Compra en 1 cuota de USD ${precioCuotas[0]}, realizada!`);
+                        vuelto = disponibleTarjeta - precioMasInteres;
+                        alert(`Le han sobrado: USD ${vuelto.toFixed(2)}`);
+                        alert("¡Volviendo al menú de productos!");
+                        opcionCuotas = 0;
+                        carrito();
+
                     break;
-                case 2:
-                    alert(`¡Compra en 3 cuotas de USD ${precioCuotas[2]}, realizada!`);
-                    vuelto = disponibleTarjeta - precioMasInteres;
-                    alert(`Le han sobrado: USD ${vuelto.toFixed(2)}`);
-                    alert("¡Volviendo al menú de productos!");
-                    opcionCuotas = 0;
-                    carrito();
+                    
+                    case 2:
+                        
+                        alert(`¡Compra en 3 cuotas de USD ${precioCuotas[2]}, realizada!`);
+                        vuelto = disponibleTarjeta - precioMasInteres;
+                        alert(`Le han sobrado: USD ${vuelto.toFixed(2)}`);
+                        alert("¡Volviendo al menú de productos!");
+                        opcionCuotas = 0;
+                        carrito();
+                        
                     break;
-                case 3:
-                    alert(`¡Compra en 6 cuotas de USD ${precioCuotas[5]}, realizada!`);
-                    vuelto = disponibleTarjeta - precioMasInteres;
-                    alert(`Le han sobrado: USD ${vuelto.toFixed(2)}`);
-                    alert("¡Volviendo al menú de productos!");
-                    opcionCuotas = 0;
-                    carrito();
+                    
+                    case 3:
+                        
+                        alert(`¡Compra en 6 cuotas de USD ${precioCuotas[5]}, realizada!`);
+                        vuelto = disponibleTarjeta - precioMasInteres;
+                        alert(`Le han sobrado: USD ${vuelto.toFixed(2)}`);
+                        alert("¡Volviendo al menú de productos!");
+                        opcionCuotas = 0;
+                        carrito();
+                        
                     break;
-                case 4:
-                    alert(`¡Compra en 12 cuotas de USD ${precioCuotas[11]}, realizada!`);
-                    vuelto = disponibleTarjeta - precioMasInteres;
-                    alert(`Le han sobrado: USD ${vuelto.toFixed(2)}`);
-                    alert("¡Volviendo al menú de productos!");
-                    opcionCuotas = 0;
-                    carrito();
+                    
+                    case 4:
+                        
+                        alert(`¡Compra en 12 cuotas de USD ${precioCuotas[11]}, realizada!`);
+                        vuelto = disponibleTarjeta - precioMasInteres;
+                        alert(`Le han sobrado: USD ${vuelto.toFixed(2)}`);
+                        alert("¡Volviendo al menú de productos!");
+                        opcionCuotas = 0;
+                        carrito();
+                        
                     break;
                 
-                case 0:
-                    alert("¡Volviendo al menú de pagos!");
-                    pago();
-            }
+                    case 0:
+                        
+                        alert("¡Volviendo al menú de pagos!");
+                        pago();
+
+                    break;
+                    
+                }
+
             } while (opcionCuotas != 0);
             
     }
@@ -225,6 +289,7 @@ console.log("===============================")
 alert("¡Bienvenido a Banifox!");
 
 do {
+
     opcionMenu = Number(prompt(`Seleccione una opción: \n
     1) Ingresar \n
     2) Registrarse \n
@@ -232,33 +297,51 @@ do {
     (Aclaración: Solo puede registrar un único usuario)`));
     
     switch (opcionMenu) {
+
         case 1:
+
             let usuario = prompt("Ingrese su usuario: ");
             let contrasena = prompt("Ingrese su contraseña: ");
-            if ((usuario == usuarios && usuarios != "") && (contrasena == contrasenas && contrasenas != "") ) {
+
+            if ((usuario == usuarios && usuarios != "") && (contrasena == contrasenas && contrasenas != "")) {
+                
                 alert("¡Ingresaste correctamente!");
                 carrito();
+
             } else {
+
                 alert("El usuario y/o contraseña son incorrectos");
+
             }
+
             break;
         
         case 2:
+
             let nuevoUsuario = prompt("Ingrese un usuario: ");
             let nuevaContrasena = prompt("Ingrese una contraseña: ");
+
             if ((nuevoUsuario != "") && (nuevaContrasena != "")) {
+
                 usuarios.push(nuevoUsuario);
                 contrasenas.push(nuevaContrasena);
+
             } else {
+
                 alert("No puede ingresar un espacio vacio, intentelo de nuevo");
                 opcionMenu = 2;
                 break;
+
             }
+
             console.log("Su usuario es: " + usuarios + " Y su contraseña es: " + contrasenas );
             break;
         
         case 0:
+
             alert("¡Hasta luego!");
+
     }
+
 }while (opcionMenu != 0);
 
